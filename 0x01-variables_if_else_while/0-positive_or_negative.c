3df595d695f3fc82c1bdc5e069f95b81819b6fbf
root <nafisatoyebola@gmail.com>
2023-02-20 21:47:58 -0800

    ifelsewhile

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- * print whether the number stored in the variable
- * n is positive or negative
+/**
+ * main - prints whether the number stored in the variable
+ *        n is positive or negative
+ *        Return: Always 0
  * /
 int main(void)
 {
@@ -12,12 +13,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
- while (n > 5)
+ if (n > 5)
 {
-("print the last digit of n is greater than 5/n");
-while (n = 0)
-("print the last digit of n is 0/n");
-while (n<6 != 0)
-("print the last digit of n is less than 6 and not equal to 0/n");
+printf("the last digit of n is greater than 5/n");}
+else if (n = 0)
+{
+printf(" the last digit of n is 0/n");}
+else if (n<6 != 0)
+{
+printf("the last digit of n is less than 6 and not equal to 0/n");
 	return (0);}
 }

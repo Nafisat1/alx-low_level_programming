b37f6975dc40b7e387308512521437576c82d437
root <nafisatoyebola@gmail.com>
2023-02-23 05:22:31 -0800

    functions

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit: Returns the value of the last digit
+ *@n: input number
+
+ * Return: Always 0
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+	_putchar(-l + '0');
+	return (-l);
+	}
+	else
+	{
+		-putchar(l + '0');
+		return (l);
+	}
+}		

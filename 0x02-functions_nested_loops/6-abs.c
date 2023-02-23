8b8f1876fdf1bbbd3818e46b46f5c28eef4279a1
root <nafisatoyebola@gmail.com>
2023-02-23 03:53:18 -0800

    functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @r: The integer to be computed.
+ *
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	if r == -1;
+	{
+		printf("%d\n", r);
+	}
+	else if r == 0
+	{
+		printf("%d\n", r);
+	}
+	else if r == 1;
+	{
+		printf("%d\n", r);
+	}
+       else 
+	{
+	printf("%d\n", r);
+	}
+	return (0);
+}

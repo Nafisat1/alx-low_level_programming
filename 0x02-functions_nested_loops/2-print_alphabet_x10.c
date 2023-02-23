6fb2570e27354b9c9e3f916bf7bc36dd726d68aa
root <nafisatoyebola@gmail.com>
2023-02-23 00:40:51 -0800

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * follwed by a new line
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+	{
+
+	for (j = i; i <= 10; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	}
+}
+

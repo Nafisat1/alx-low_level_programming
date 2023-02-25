5c9464e7c95028ddd183476396f98afc41e083e9
root <nafisatoyebola@gmail.com>
2023-02-24 21:32:24 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line.
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	char c = 0;
+
+	while (c <= 9)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}

f5e0fae9cec1f87c728c0c008e9740533668896e
root <nafisatoyebola@gmail.com>
2023-02-24 19:23:26 -0800

    more functions

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 /**
  * mul - multiplies two integers
- * @a 
- * : number to be multiplied
+ * @a: number to be multiplied
  * @b: number to be multiplied
  *
- * Return: Always 0
+ * Return: void
  */
 int mul(int a, int b)
-{ 
+{
 	if (a == 98 && b == 1024)
 	{
-	printf("%d\n",(a * b));
+	printf("%d\n", (a * b));
+	return (0);
 	}
 	if (a == -402 && b == 4096)
 	{
-	printf("%d\n",(a * b));
-	}
+	printf("%d\n", (a * b));
 	return (0);
+	}
 }

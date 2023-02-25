98fd4b7e3ec4a042d73d37323b8ba018fd027bad
root <nafisatoyebola@gmail.com>
2023-02-25 01:52:41 -0800

    most functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line.
+ * @c: is an integer
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+	if (c != '2' && c != '4')
+	{
+	_putchar(c);
+	}
+	c++;
+	}
+	_putchar('\n');
+}

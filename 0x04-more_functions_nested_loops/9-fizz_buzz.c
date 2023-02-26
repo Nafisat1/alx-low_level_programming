123da547b6de2eb2f15af62790ce87e35a3adeec
root <nafisatoyebola@gmail.com>
2023-02-26 08:15:43 -0800

    more functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,12 +14,12 @@ int main(void)
 	i = 1;
 	while (i <= 100)
 	{
+	if (i % 3 != 0 && i % 5 != 0)
+	printf("FizzBuzz");
 	if (i % 3 == 0)
 	printf("Fizz");
 	else if (i % 5 == 0)
 	printf("Buzz");
-	else if (i % 3 != 0 && i % 5 != 0)
-	printf("FizzBuzz");
 	else 
 	printf("%d", i);
 

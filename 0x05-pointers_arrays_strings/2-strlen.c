e48c1b37f2611fe6ee9de608dcd65075e367096a
root <nafisatoyebola@gmail.com>
2023-02-27 13:56:34 -0800

    strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * __strlen - returns the length of a string
+ * @s: the string
+ *
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+	return (len);
+}

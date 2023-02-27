1444dd9bc4701c67f17fbb754d140a7549354aac
root <nafisatoyebola@gmail.com>
2023-02-27 15:00:37 -0800

    strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,11 +9,11 @@
 void _puts(char *str)
 {
 	int i = 0;
-	char c;
 
-	while (c = *(str + i) != '\0')
+	while (str[i] != '\0')
 	{
-		_putchar(c);
+		_putchar(str[i]);
 		i++;
 	}
+	_putchar('\n');
 }

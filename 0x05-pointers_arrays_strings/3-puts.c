5c6a546c68f9f16e145fb1d1bdaa66623fba5119
root <nafisatoyebola@gmail.com>
2023-02-27 14:32:51 -0800

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: string to be printed
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int s = *str;
+
+	_putchar('s');
+	_putchar('\n');
+}

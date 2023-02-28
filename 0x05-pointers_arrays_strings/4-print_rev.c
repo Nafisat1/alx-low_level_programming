6287f570645837f1e694684b0fd535f4947b5ef2
root <nafisatoyebola@gmail.com>
2023-02-28 06:11:05 -0800

    strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,26 +3,26 @@
 /**
  * rev_string - reverses a string
  * @s: string to be reversed
+ *
+ * Return: Always 0
  */
 void rev_string(char *s)
 {
-	char tmp;
-	int i, len, len1;
+	int i, j, len;
 
-	len = 0;
-	len1 = 0;
+	i = 0;
 
-	while (s[len] != '\0')
+	while (s[i] != '\0')
 	{
-		len++;
+		i++;
 	}
 
-	len1 = len - 1;
+	len = i;
 
-	for (i = 0; i < len / 2; i++)
+	for (j = len - 1; j >= 0; j--)
 	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
+		_putchar(s[j]);
 	}
+
+	_putchar('\n');
 }

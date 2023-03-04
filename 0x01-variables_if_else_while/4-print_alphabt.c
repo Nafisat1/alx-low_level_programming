d0364dd8450d49350617f1ab22581b973920f076
root <nafisatoyebola@gmail.com>
2023-03-03 17:23:38 -0800

    ifelsewhile

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,11 +7,14 @@
  */
 int main(void)
 {
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	       if (ch != 'q' && ch != 'e')
-	       putchar(ch);
-	putchar('\n');
+	char i;
+	{
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+	if (i != 'q' && i != 'e')
+	       putchar(i);
+	}
+	}
+	putchar("\n");
 	return (0);
 }

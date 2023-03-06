6786cff5c8be6c8ad50d7614e82d02414b4e786a
root <nafisatoyebola@gmail.com>
2023-03-06 13:20:17 -0800

    strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string where character is located
+ * @c: character to be located
+ *
+ * Return: a pointer to the first occurrence of the character c
+ *         NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}

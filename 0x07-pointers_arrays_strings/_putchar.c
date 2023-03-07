bb5f36ae223c4620c8f0d7651ff37ffcab78b70b
Nafisat1 <nafisatoyebola@gmail.com>
2023-03-07 03:44:52 -0800

    pointers

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *int _putchar - writes the character c to stdout
+ *@c: The character to print
+ *
+ * Return: Always 0
+ */
+int _putchar(char c)
+{
+        return (0);
+}

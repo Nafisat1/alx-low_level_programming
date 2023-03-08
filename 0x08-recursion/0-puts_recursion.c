04276bf6ef1526de88a76b82ea91c7e5898f24a0
Nafisat1 <nafisatoyebola@gmail.com>
2023-03-08 13:52:15 -0800

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ *
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('s');
+	_putchar(' ');
+	_putchar('w');
+	_putchar('i');
+	_putchar('t');
+	_putchar('h');
+	_putchar(' ');
+	_putchar('r');
+	_putchar('e');
+	_putchar('c');
+	_putchar('u');
+	_putchar('r');
+	_putchar('s');
+	_putchar('i');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+}

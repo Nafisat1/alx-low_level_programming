bf3be20a318078eb93389ee47bb9488bc738b72e
Nafisat1 <nafisatoyebola@gmail.com>
2023-03-08 15:00:45 -0800

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of numbers
+ * @n: the number
+ *
+ * Return: Always 0
+ * 	   -1 if n is less than 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (1);
+	return (n * factorial(n - 1));
+}

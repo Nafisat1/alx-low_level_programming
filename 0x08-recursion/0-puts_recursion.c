83f72d84a90945bdf9930bf47238f91a50468efd
Nafisat1 <nafisatoyebola@gmail.com>
2023-03-08 14:03:32 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,29 +2,18 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
+ * @s: string to print
  *
  * Return: Always 0
  */
 void _puts_recursion(char *s)
 {
-	_putchar('P');
-	_putchar('u');
-	_putchar('t');
-	_putchar('s');
-	_putchar(' ');
-	_putchar('w');
-	_putchar('i');
-	_putchar('t');
-	_putchar('h');
-	_putchar(' ');
-	_putchar('r');
-	_putchar('e');
-	_putchar('c');
-	_putchar('u');
-	_putchar('r');
-	_putchar('s');
-	_putchar('i');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }

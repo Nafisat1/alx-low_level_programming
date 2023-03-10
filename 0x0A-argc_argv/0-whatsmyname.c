6bd22bab8cea67d0b8a51b510c3f76238c889573
Nafisat1 <nafisatoyebola@gmail.com>
2023-03-10 13:08:28 -0800

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Writes a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+~{
+	printf("%s\n", *argv);
+
+	return (0);
+}

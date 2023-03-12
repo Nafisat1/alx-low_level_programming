21f07943df86e4d1c588cc210343a2359fd0e65a
Nafisat1 <113612636+Nafisat1@users.noreply.github.com>
2023-03-12 09:01:26 -0700

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,10 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * *_memcpy - copies memory area
  * @dest: destination
  * @src: source to copy from
  * @n: length of src
-=======
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
->>>>>>> 1427087f0d0a15f3169434f41b0b3ccaa088cc18
  *
  * Return: pointer to dest.
  */

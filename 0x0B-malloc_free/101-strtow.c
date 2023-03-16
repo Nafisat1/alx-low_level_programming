fb720bfc275b26ba1445b6c8ae4e3c3926d87068
Nafisat1 <nafisatoyebola@gmail.com>
2023-03-15 20:27:34 -0700

    split

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split into words
+ *
+ * Return: NULL if str == NULL or str == "" and if function fails
+ *         a pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+
+}

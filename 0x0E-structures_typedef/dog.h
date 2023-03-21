d80faa4f4711746d7d309722de9c3f9f489f1e5b
Nafisat1 <nafisatoyebola@gmail.com>
2023-03-20 20:30:30 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - prints the information of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;

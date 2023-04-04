1ed13bcec0c4e1a25edd3b7c1e3fad19be8c54c9
Nafisat1 <nafisatoyebola@gmail.com>
2023-04-04 03:03:59 -0700

    advanced task

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,16 +9,16 @@
 int main(void)
 {
 
-        int n1 = 0, n2 = 1, next = 0, sum = 0;
+	int n1 = 0, n2 = 1, next = 0, sum = 0;
 
-        while (next < 4000000)
-        {
-                next = n1 + n2;
-                n1 = n2;
-                n2 = next;
-		if (next % 2 == 0)
-			sum += next;
-        }
-        printf("%i\n", sum);
-        return (0);
+	while (next < 4000000)
+	{
+	next = n1 + n2;
+	n1 = n2;
+	n2 = next;
+	if (next % 2 == 0)
+	sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
 }

d40d8becc5a6f2dd669c2535d2dc9554fa0e504d
Nafisat1 <nafisatoyebola@gmail.com>
2023-04-03 18:59:05 -0700

    advanced task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -  computes and prints the sum of all the multiples of 3 or 5
+ *         below 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	while(i < 10)
+	{
+	if (i % 3 == 0 & i % 5 == 0)
+		printf("%d", i);
+	i++;
+	}
+}

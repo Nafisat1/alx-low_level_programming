89b9e219ccc4e03280e3d20128efe571456e9e9f
Nafisat1 <nafisatoyebola@gmail.com>
2023-04-04 07:13:15 -0700

    corrections

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,32 +3,28 @@
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14,
  * followed by a new line.
- * @c: character to be printed
- * @j: character to be computed
  *
  *  Return: Always 0
  */
 void more_numbers(void)
 {
-	char c = 0;
-	int j;
+	char a, c;
+	int i = 0;
 
-	while (c <= 10)
+	while (i < 10)
 	{
-		j = 0;
-		while (j < 10)
+		for (a = 0; a <= 14; a++)
 		{
-			_putchar(c);
-			j++;
+			c = a;
+			if (a > 9)
+			{
+				_putchar('1');
+				c = a % 10;
+			}
+			_putchar('0' + c);
 		}
 
-		j = 10;
-		while (j < 15)
-		{
-			_putchar(c);
-			j++;
-		}
-	_putchar('\n');
-	c++;
+		_putchar('\n');
+		i++;
 	}
 }

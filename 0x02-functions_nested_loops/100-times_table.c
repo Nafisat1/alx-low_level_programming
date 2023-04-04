fcb0f2694bd158e9d5f1367a1710f0e2c1ae042b
Nafisat1 <nafisatoyebola@gmail.com>
2023-04-03 18:24:35 -0700

    advanced task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: the times table to be printed
+ *
+ * Return: Always 0
+ */
+void print_times_table(int n)
+{
+	while ((n > 0) && (n < 15))
+		n++;
+	_putchar('n');	
+}
